e2b3a44b0759d8d0aa21b2970bac9a12e6317498
Rudratej Chilkewar <rudratejchilkewar@gmail.com>
2023-04-19 21:54:21 +0530

    Delete src directory

--- src/Components/Shared/Card/Card.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./Card.module.css";
-
-const Card = ({ children, className }) => {
-  return <div className={` ${className} ${styles.card} `}>{children}</div>;
-};
-
-export default Card;
